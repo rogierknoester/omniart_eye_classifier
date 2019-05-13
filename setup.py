883dda2de9ca801402d7748085263aaa0c855db7
Rogier Knoester <knoesterrogier@gmail.com>
2019-05-13 18:07:29 +0200

    fix setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
 setuptools.setup(name='omniart_eye_classifier',
-                 version='0.1',
+                 version='0.1.2',
                  description='A CNN that can classify eyes by their respective colour',
-                 url='http://github.com/rogierkn/omniart_eye_classifier',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url='http://github.com/rogierknoester/omniart_eye_classifier',
                  author='Rogier Knoester',
                  author_email='knoesterrogier+omniart@gmail.com',
                  license='MIT',
-                 packages=setuptools.find_packages,
+                 packages=setuptools.find_packages(),
                  zip_safe=False)
